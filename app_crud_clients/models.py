d0230cf055c70ef9065a536c3b74a3067175b0b5
Caio Adriano <caiobhadriano@gmail.com>
2023-12-20 11:27:20 -0300

    [Feat]: Showing all users into list

--- app_crud_clients/models.py
+++ app_crud_clients/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-
-class usuario(models.Model):
+class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(name="nome", blank=False, db_column="nome")
     email = models.EmailField(name="email", blank=False, db_column="email", unique=True)
--- app_crud_clients/views.py
+++ app_crud_clients/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from . import models
 def home(request):
     return render(request=request, template_name="usuarios/home.html")
 
 def usuarios(request):
-    return render(request=request, template_name="usuarios/listagem_usuarios.html")
+    novo_usuario = models.Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.email = request.POST.get('email')
+    novo_usuario.save()
+    
+    usuarios = {
+        'usuarios': models.Usuario.objects.all(),
+    }
+    
+    return render(request=request, template_name="usuarios/listagem_usuarios.html", context=usuarios)
     
