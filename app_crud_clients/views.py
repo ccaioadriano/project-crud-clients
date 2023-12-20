bdc87e09ba3bba8035380885927cec5f56fe4e1e
Caio Adriano <caiobhadriano@gmail.com>
2023-12-20 10:35:49 -0300

    [Feat]: Creating route for listing to users

--- app_crud_clients/views.py
+++ app_crud_clients/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-
 def home(request):
     return render(request=request, template_name="usuarios/home.html")
+
+def usuarios(request):
+    return render(request=request, template_name="usuarios/listagem_usuarios.html")
     
--- project_crud_clients/urls.py
+++ project_crud_clients/urls.py
@@ -3,4 +3,8 @@ from app_crud_clients import views
 urlpatterns = [
     # <dominio.com>/...
     path(route="", view=views.home, name="home"),
+    
+    #usuários
+    path(route="usuarios/", view=views.usuarios, name="listagem_usuarios"),
+    
 ]
