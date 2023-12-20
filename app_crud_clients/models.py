4ef3a3fba6d3254046a7a1a79224b2efe40296ba
Caio Adriano <caiobhadriano@gmail.com>
2023-12-20 10:34:56 -0300

    [Feat]: Creating model user

--- app_crud_clients/models.py
+++ app_crud_clients/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(name="nome", blank=False, db_column="nome")
+    email = models.EmailField(name="email", blank=False, db_column="email", unique=True)
+    
